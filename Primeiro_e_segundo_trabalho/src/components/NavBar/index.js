22729d2b5b2b912d09bc755a5ff7b4b6c52cf8b2
Heron <84138510+szHeron@users.noreply.github.com>
2021-12-20 15:10:41 -0300

    Update index.js

--- Primeiro_e_segundo_trabalho/src/components/NavBar/index.js
+++ Primeiro_e_segundo_trabalho/src/components/NavBar/index.js
@@ -3,6 +3,7 @@ import { useHistory } from 'react-router';
 
 export function NavBar(){
     const history = useHistory();
+    
     return(
         <div className="navbar">
             <button onClick={()=>history.push("/")}>1º Trabalho</button>
